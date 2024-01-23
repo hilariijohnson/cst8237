5ee554407b8f08d5faeb974e0fb13611fc2072be
Hilariii <40604581+hilariijohnson@users.noreply.github.com>
2024-01-23 16:06:28 -0500

    Delete Source directory

--- Source/UE4Lab1/UE4Lab1.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE4Lab1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4Lab1, "UE4Lab1" );
